540a5a9fb473235d74e237e425491ec02d414d31
bycra <bycra@jinwoo.local>
2023-10-23 22:03:04 +0200

    wlh jsais plus

--- draft.py
+++ draft.py
@@ -1,83 +1,31 @@
-subway_data = {
-    'stations': {},
-    'join': {
-        's1': ['station1', 'station2', 'station3', 'station1'],
-        's2': ['station2', 'station3', 'station1', 'station4'],
-        'lon': [5, 10, 15, 20]  # Assuming these are the times
-    }
-}
+from collections import defaultdict
+from data import subway_data
 
-#  Python3 program for Bellman-Ford's single source
-# shortest path algorithm.
- 
-# Class to represent a graph
- 
 class Graph:
- 
-    def __init__(self, vertices):
-        self.V = vertices  # No. of vertices
-        self.graph = []
- 
-    # function to add an edge to graph
-    def addEdge(self, u, v, w):
-        self.graph.append([u, v, w])
- 
-    # utility function used to print the solution
-    def printArr(self, dist):
-        print("Vertex Distance from Source")
-        for i in range(self.V):
-            print("{0}\t\t{1}".format(i, dist[i]))
- 
-    # The main function that finds shortest distances from src to
-    # all other vertices using Bellman-Ford algorithm. The function
-    # also detects negative weight cycle
-    def BellmanFord(self, src):
- 
-        # Step 1: Initialize distances from src to all other vertices
-        # as INFINITE
-        dist = [float("Inf")] * self.V
-        dist[src] = 0
- 
-        # Step 2: Relax all edges |V| - 1 times. A simple shortest
-        # path from src to any other vertex can have at-most |V| - 1
-        # edges
-        for _ in range(self.V - 1):
-            # Update dist value and parent index of the adjacent vertices of
-            # the picked vertex. Consider only those vertices which are still in
-            # queue
-            for u, v, w in self.graph:
-                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
-                    dist[v] = dist[u] + w
- 
-        # Step 3: check for negative-weight cycles. The above step
-        # guarantees shortest distances if graph doesn't contain
-        # negative weight cycle. If we get a shorter path, then there
-        # is a cycle.
- 
-        for u, v, w in self.graph:
-            if dist[u] != float("Inf") and dist[u] + w < dist[v]:
-                print("Graph contains negative weight cycle")
-                return
- 
-        # print all distance
-        self.printArr(dist)
- 
- 
-# Driver's code
-if __name__ == '__main__':
-    g = Graph(5)
-    g.addEdge(0, 1, -1)
-    g.addEdge(0, 2, 4)
-    g.addEdge(1, 2, 3)
-    g.addEdge(1, 3, 2)
-    g.addEdge(1, 4, 2)
-    g.addEdge(3, 2, 5)
-    g.addEdge(3, 1, 1)
-    g.addEdge(4, 3, -3)
- 
-    # function call
-    g.BellmanFord(0)
- 
-# Initially, Contributed by Neelam Yadav
-# Later On, Edited by Himanshu Garg
+    def __init__(self):
+        self.edges = defaultdict(list)
+        self.weights = {}
+        self.add_edge(subway_data)
 
+    def add_edge(self, subway_data):
+        for i in range(len(subway_data['join']['summit1'])):
+            from_node = subway_data['join']['summit1'][i]
+            to_node = subway_data['join']['summit2'][i]
+            weight = subway_data['join']['lon'][i]
+        
+            self.edges[from_node].append(to_node)
+            self.weights[(from_node, to_node)] = weight
+            self.edges[to_node].append(from_node)
+            self.weights[(to_node, from_node)] = weight
+
+    def bellman_ford(self, start_node):
+        pass
+
+# Create a graph instance
+graph = Graph()
+
+    
+print(graph.edges, graph.weights)
+
+
+# print(graph.bellman_ford('276'))
--- graph.py
+++ graph.py
@@ -5,19 +5,21 @@ from data import subway_data
 # Création du graphe où on ajoute les arrêtes et les poids 
 class Graph():
     def __init__(self):
-        # Initialisation de la classe Graph avec des dictionnaires pour stocker les arêtes et les poids
         self.edges = defaultdict(list)
         self.weights = {}
-    
-    def add_edge(self, from_node, to_node, weight):
-        # Méthode pour ajouter une arête avec son poids, en supposant que les arêtes sont bidirectionnelles
-        # On ajoute l'arête du nœud de départ au nœud d'arrivée et vice-versa
-        self.edges[from_node].append(to_node)
-        self.edges[to_node].append(from_node)
+        self.add_edge(subway_data)
+
+    def add_edge(self, subway_data):
+        for i in range(len(subway_data['join']['summit1'])):
+            from_node = subway_data['join']['summit1'][i]
+            to_node = subway_data['join']['summit2'][i]
+            weight = subway_data['join']['lon'][i]
         
-        # On enregistre le poids de l'arête dans le dictionnaire des poids
-        self.weights[(str(from_node), str(to_node))] = weight
-        self.weights[(to_node, from_node)] = weight
+            self.edges[from_node].append(to_node)
+            self.weights[(from_node, to_node)] = weight
+            self.edges[to_node].append(from_node)
+            self.weights[(to_node, from_node)] = weight
+
     
     def is_connected(self):
         if not self.edges:
@@ -33,34 +35,44 @@ class Graph():
             for neighbor in self.edges[sommet]:
                 if neighbor not in visited and neighbor not in queue:
                     queue.append(neighbor)
-        return len(visited) == len(self.edges)
+        return len(visited) == len(self.edges) 
     
-    def bellman_ford(self, start, end):
+    def bellman_ford(self, start_node):
         pass
 
 
 # Création de l'instance graphe
 graph = Graph()
 
-# Création d'une liste d'arêtes et de leurs poids
-edges = list((subway_data['join']['summit1'][i],subway_data['join']['summit2'][i],subway_data['join']['lon'][i])
-                    for i in range(len(subway_data['join']['summit1'])))
-
-# Ajout des arêtes au graphe
-for edge in edges:
-    graph.add_edge(*edge)
-
-print(graph.edges ,'\n\n', graph.weights)
+# Check connectivity
+is_connected = graph.is_connected()
+if is_connected:
+    print("Le graphe est connecté.")
+else:
+    print("Le graphe n'est pas connecté.")
 
 
 
-# fonction qui trouve l'id d'une station fonction du nom
-def find_id(summit):
-    sommet = summit.get().lower()
-    for  key in subway_data['stations']:
-        if subway_data['stations'][key][0] == sommet:
-            node_id = key
-    return node_id 
+
+# # print(graph.edges ,'\n\n', graph.weights)
+
+# start_node = 0  # Replace with the desired starting node
+# shortest_paths = graph.bellman_ford(start_node)
+
+# # print(shortest_paths)
+# # Print the shortest paths
+# for node, distance in shortest_paths.items():
+#     print(f"Distance from {start_node} to {node}: {distance}")
+
+
+
+# # fonction qui trouve l'id d'une station fonction du nom
+# def find_id(summit):
+#     sommet = summit.get().lower()
+#     for  key in subway_data['stations']:
+#         if subway_data['stations'][key][0] == sommet:
+#             node_id = key
+#     return node_id 
 
 
 
