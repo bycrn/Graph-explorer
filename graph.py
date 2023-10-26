13a3b94a5d0c0dd1429976365998db771c89f0fc
bycra <bycra@jinwoo.local>
2023-10-26 13:29:44 +0200

    main creation

--- graph.py
+++ graph.py
@@ -92,24 +92,6 @@ class Graph():
 
 
 
-if __name__ == "__main__":
-    # Create a graph instance
-    graph = Graph()
-    # print(graph.edges,'\n', graph.weights)
-    
-    # Check connectivity
-    is_connected = graph.is_connected()
-    if is_connected:
-        print("Le graphe est connecté.")
-    else:
-        print("Le graphe n'est pas connecté.")
-    
-    start_node = 48
 
-    graph.bellman_ford(start_node)
-    
-    print(graph.prim(start_node))
-
-    # Print the calculated distances
 
 
