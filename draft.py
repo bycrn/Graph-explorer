4051a70771772ce6241823d5c6c321c0daac8eb1
bycra <bycra@jinwoo.local>
2023-10-26 02:55:40 +0200

    bellman done in the draft branch dev

--- draft.py
+++ draft.py
@@ -6,6 +6,8 @@ class Graph:
         self.edges = defaultdict(list)
         self.weights = {}
         self.add_edge(subway_data)
+        self.d = {}
+    
 
     def add_edge(self, subway_data):
         for i in range(len(subway_data['join']['summit1'])):
@@ -17,15 +19,61 @@ class Graph:
             self.weights[(from_node, to_node)] = weight
             self.edges[to_node].append(from_node)
             self.weights[(to_node, from_node)] = weight
+    
+    def calculateNbrVertex(self):
+        max_vertex = max(self.edges.keys())
+        return max_vertex
+        
+
+# Bellman-Ford 
+
+# En entrée :  G=(V,E) graphe orienté pondéré et r un sommet de G
+# En sortie : Pour chaque sommet u de G un chemin de poids minimal de r vers u
+
+# Initialisations
+# Pour i:= 1 à n-1 /* n = nbre de sommets */
+#   Relacher tous les arcs de G
+# Vérifier qu'il n'y a pas de circuit négatif
+
+#Initialisation 
+    # Mettre une etiquette inf sur tous les sommets et une etiquette 0 sur r 
+
+# Relâchement(u,v)
+# Si d[v] > d[u] + w(u,v) alors
+    # d[v] := d[u] + w(u,v)
+    # Parent[v] := u
+
 
     def bellman_ford(self, start_node):
-        pass
+        #Initialisation
+        self.d = {node: float('inf') for node in self.edges}
+        self.d[start_node] = 0
+        #Nombre de sommets
+        nbrVertex = self.calculateNbrVertex()
+        for i in range(1, nbrVertex - 1):
+            for from_node, to_nodes in self.edges.items():
+                for to_node in to_nodes:
+                    if self.d[to_node] > self.d[from_node] + self.weights[(from_node, to_node)]:
+                        self.d[to_node] = self.d[from_node] + self.weights[(from_node, to_node)]
+                
 
 # Create a graph instance
-graph = Graph()
 
+# struct of graph edges :{0: [238, 159], 238: [0, 322, 239], .....}
+# struct of graph weight = {(s1, S2) : w1, ... , (sk, sn) : wp }
+
+
+
+
+if __name__ == "__main__":
+    # Create a graph instance
+    graph = Graph()
+    # print(graph.edges,'\n', graph.weights)
     
-print(graph.edges, graph.weights)
+    start_node = 48
 
+    graph.bellman_ford(start_node)
 
-# print(graph.bellman_ford('276'))
+    # Print the calculated distances
+    print(graph.d)
+
